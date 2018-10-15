ea5ad81f048b109e926f836b542d0abae4338078
湊 <minato.nekurochino@gmail.com>
2018-10-15 09:47:09 +0900

    ファイルとか色々

---
+++ Assets/kozin/minato/script/Cameracontrol.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cameracontrol : MonoBehaviour
+{
+
+    public Camera MainCamera;
+    public Camera SubCamera;
+
+    private bool MainCameraON = true;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        MainCamera.enabled = true;
+        SubCamera.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) && MainCameraON == true)
+        {
+            MainCamera.enabled = false;
+            SubCamera.enabled = true;
+
+            MainCameraON = false;
+        }else if(Input.GetMouseButtonDown(1) && MainCameraON == false)
+        {
+            MainCamera.enabled = true;
+            SubCamera.enabled = false;
+
+            MainCameraON = true;
+        }
+    }
+}
---
+++ Assets/kozin/minato/script/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(0.1f, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        if (Input.GetKey("w"))
+        {
+            transform.position += new Vector3(0, 0, 0.1f);
+        }
+        if (Input.GetKey("s"))
+        {
+            transform.position += new Vector3(0f, 0, -0.1f);
+        }
+        
+    }
+}
