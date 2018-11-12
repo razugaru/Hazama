be757de1d5b94e40daa12face416a0dd122bf07e
Ttomok1 <tomokimiki0118@gmail.com>
2018-11-12 11:35:09 +0900

    メインターゲットαバージョン完成。

---
+++ Assets/kozin/Takahashi/Script/Controll.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+
+public class Controll : MonoBehaviour {
+
+    [SerializeField]
+    private Transform m_target = null;
+
+    private NavMeshAgent m_navAgent = null;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        m_navAgent = GetComponent<NavMeshAgent>();
+    }
+    void Start () {
+		if(m_target != null)
+        {
+            m_navAgent.destination = m_target.position;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/kozin/Takahashi/Script/Satus.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum State
+{
+    idle, move, see
+}
+public class Satus : MonoBehaviour {
+    // Use this for initialization
+    [HideInInspector]
+    private Transform player;
+    public float speed = 0.3f;
+    public State _state;
+    public float dist;
+
+    private float elapsedTime;
+
+    void Start () {
+       // player = GameObject.FindGameObjectsWithTag("Player").transform;
+        _state = State.move;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
