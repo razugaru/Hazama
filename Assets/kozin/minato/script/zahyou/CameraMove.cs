31d1ab15508d9bfc4325bcd1d854d63b9b2b4a88
湊 <minato.nekurochino@gmail.com>
2018-12-13 17:12:06 +0900

    エラー解除

---
+++ Assets/kozin/minato/script/zahyou/CameraMove.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public float speed = 4f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            transform.position += new Vector3(0.1f,0,0);
+        }
+    }
+}
---
+++ Assets/kozin/minato/script/zahyou/Score.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour {
+
+    //Zahyouクラスから変数を参照するためのもの
+    public Zahyou Zahyou;
+
+    //Scoreを計測するための変数 
+    public int Sget1 = 0; 
+    public int cnt = 0;
+
+	void Update () {
+
+        //クリックしたときにZahyouクラスからPoint1変数を参照する。
+        if (Input.GetMouseButton(0))   
+        {
+            Sget1 = Zahyou.Point1;
+            //Debug.Log(Sget1);   //Sget変数の値を出すもの、いらなければ消して
+            
+            //Point1の値が1のとき(加点とみなされてる時)に呼び出すもの
+            if (Sget1 == 1)
+            {
+                if (cnt == 0)
+                {
+                    cnt = 1;
+                }
+                //Debug.Log(cnt);
+            }
+            if (Sget1 == 0)
+            {
+                if (cnt == 1)
+                {
+                    cnt = 0;
+                }
+                //Debug.Log(cnt);
+            }
+        }
+    }
+}
