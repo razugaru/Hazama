28cede5cf38097e455ea3d99a2a1b95cf93eefda
razugaru <31424863+razugaru@users.noreply.github.com>
2018-12-13 17:28:10 +0900

    Update and rename Assets/kozin/razugaru/Score.cs to 資産/ kozin/ razugaru/Score.cs

--- 資産/ kozin/ razugaru/Score.cs
+++ 資産/ kozin/ razugaru/Score.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Score : MonoBehaviour {
 
@@ -9,32 +10,27 @@ public class Score : MonoBehaviour {
 
     //Scoreを計測するための変数 
     public int Sget1 = 0; 
-    public int cnt = 0;
 
 	void Update () {
 
         //クリックしたときにZahyouクラスからPoint1変数を参照する。
         if (Input.GetMouseButton(0))   
         {
-            Sget1 = Zahyou.Point1;
+            Sget1 = Zahyou.Score;
             //Debug.Log(Sget1);   //Sget変数の値を出すもの、いらなければ消して
             
             //Point1の値が1のとき(加点とみなされてる時)に呼び出すもの
-            if (Sget1 == 1)
+            if (Sget1 == 2)
             {
-                if (cnt == 0)
-                {
-                    cnt = 1;
-                }
-                //Debug.Log(cnt);
+                SceneManager.LoadScene("ResultA");
             }
-            if (Sget1 == 0)
+            else if (Sget1 == 1)
             {
-                if (cnt == 1)
-                {
-                    cnt = 0;
-                }
-                //Debug.Log(cnt);
+                SceneManager.LoadScene("ResultB");
+            }
+            else
+            {
+                SceneManager.LoadScene("ResultC");
             }
         }
     }
