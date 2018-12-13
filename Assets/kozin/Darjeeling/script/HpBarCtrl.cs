b738738c80a7f7ab1101785e25fd3f58d6baab70
ダージリン <rinumimaki@gmail.com>
2018-12-13 17:19:15 +0900

    メインターゲット モデル

---
+++ Assets/kozin/Darjeeling/script/HpBarCtrl.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; // ←※これを忘れずに入れる
+
+public class HpBarCtrl : MonoBehaviour
+{
+
+    Slider _slider;
+    void Start()
+    {
+        // スライダーを取得する
+        _slider = GameObject.Find("kikendo").GetComponent<Slider>();
+    }
+
+    float _hp = 0;
+    void Update()
+    {
+        // HP上昇
+        _hp += 0.001f;
+        if (_hp > 1)
+        {
+        }
+
+        // HPゲージに値を設定
+        _slider.value = _hp;
+    }
+}
---
+++ Assets/kozin/Darjeeling/script/MoveMob.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveMob : MonoBehaviour
+{
+
+    private CharacterController enemyController;
+    private Animator animator;
+    //　目的地
+    private Vector3 destination;
+    //　歩くスピード
+    [SerializeField]
+    private float walkSpeed = 1.0f;
+    //　速度
+    private Vector3 velocity;
+    //　移動方向
+    private Vector3 direction;
+    //　到着フラグ
+    private bool arrived;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        enemyController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+        destination = new Vector3(-322f, 70.2f, -0.5f);
+        velocity = Vector3.zero;
+        arrived = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!arrived)
+        {
+            if (enemyController.isGrounded)
+            {
+                velocity = Vector3.zero;
+                animator.SetFloat("Speed", 2.0f);
+                direction = (destination - transform.position).normalized;
+                transform.LookAt(new Vector3(destination.x, transform.position.y, destination.z));
+                velocity = direction * walkSpeed;
+            }
+            velocity.y += Physics.gravity.y * Time.deltaTime;
+            enemyController.Move(velocity * Time.deltaTime);
+
+            //　目的地に到着したかどうかの判定
+            if (Vector3.Distance(transform.position, destination) < 0.5f)
+            {
+                arrived = true;
+                animator.SetFloat("Speed", 0.0f);
+            }
+        }
+    }
+}
